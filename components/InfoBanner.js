29dfcce925e9b4a0ae8409cb7666e005cdb4153e
Honza Klimeš <johny.klimes@seznam.cz>
2023-03-01 20:57:52 +0100

    feat: adding InfoBanner component

--- components/InfoBanner.js
+++ components/InfoBanner.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 function InfoBanner() {
-  return <div>InfoBanner</div>;
+  return (
+    <div className="text-center relative bg-[#201E1E] rounded-3xl m-5  hover:border-main-green hover:border-4 border-4 border-transparent">
+      <div className="text-3xl font-bold pt-2">Děláme co nás baví</div>
+      <div className="sm:pl-36 pt-3 sm:pr-36">
+        Naše rodinná firma vznikla v březnu 2017, ale opíráme se o více jak
+        třicetiletou zkušenost ve výrobě. Zabýváme se výrobou přesných rotačních
+        dílců. Hlavní činností je zakázková výroba rotačních dílců, jak kusových
+        tak i sériových, z tyče i polotovaru.
+      </div>
+      <div className="p-5 text-l font-bold">
+        Vyrábíme díly dle požadavků zákazníka do různých odvětví průmyslu.
+      </div>
+      <div className="pb-5 text-2xl font-bold">
+        Náš cíl je dodávat dílce v maximální kvalitě a požadovaných termínech.
+      </div>
+    </div>
+  );
 }
 
 export default InfoBanner;
